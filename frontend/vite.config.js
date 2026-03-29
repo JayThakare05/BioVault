a2f4c2eee1f509baf9c205970bbf25251e070d85
JayThakare05 <thakarejay2005@gmail.com>
2026-03-29 16:59:12 +0530

    biometrics added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  server: {
+    allowedHosts: [
+      '1f8b95693385e8.lhr.life'
+    ]
+  }
 })
